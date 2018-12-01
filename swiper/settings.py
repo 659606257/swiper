722da46a3479b16ad606749993d19e1ce446962f
Seamile <lanhuermao@gmail.com>
2018-12-01 19:42:01 +0800

    添加了系统脚本

--- swiper/settings.py
+++ swiper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(mo55l7=3-+dkxwey65b$ir*q&34606-(01ji7^($zzhwhe09g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
